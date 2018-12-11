825b7b730c6da8e1bdd4f0587b5117c76289f1f3
danilo.santos <danilo.santos@intravca-003892.intravca.ifba>
2018-12-11 14:46:41 -0300

    Porta detectada automaticamente.

--- AllSensors/src/conexao.py
+++ AllSensors/src/conexao.py
@@ -9,38 +9,39 @@ import serial   # pip install pyserial
 import time
 import urllib.request
 import urllib.parse
+import serial.tools.list_ports
+ports = list(serial.tools.list_ports.comports())
+
 debug = True
-
-intervalo = 300 # Intervalo entre as mediÃ§Ãµes em segundos
+intervalo = 300 # Intervalo entre as medições em segundos
 ser = 0
-
 while True:
-        try:
-            # Abre a porta serial, caso nÃ£o tenha sido aberta antes
-            if not ser:
-                ser = serial.Serial('COM16', 9600, timeout=0)
-
-            # Faz a leitura e tratamento dos dados
-            dados = ser.readline()
-            linha = dados.decode("ansi")
-            if ";" in linha:
-                temp, um = linha.split(";")
-                temp = eval(temp)
-                um = eval(um)
-                chuva = 0    # Para uso futuro
-                if debug:
-                    print("--\nTemperatura: %d ÂºC\nUmidade: %d %%\nChuva: %d" % (temp, um, chuva))
-
-                # Monta a URL e envia os dados para o webservice
-                params = urllib.parse.urlencode({'temp': temp, 'hum': um, 'rain': chuva})
-                url = "http://just.pro.br/ifba_sensors.py?%s" % params
-                conteudoweb = urllib.request.urlopen(url)
-                time.sleep(intervalo - 1)
-                
-                # Limpa o buffer de entrada para fazer uma nova leitura
-                ser.flushInput()
-        except serial.SerialException:
-            print("Erro ao fazer leitura serial.")
-        except urllib.error.URLError:
-            print("Erro ao fazer a requisiÃ§Ã£o HTTP.")
-        time.sleep(1)
+    try:
+        # Abre a porta serial, caso não tenha sido aberta antes
+        if not ser:
+            for p in ports:
+                if "Arduino" in p[1]: 
+                    ser = serial.Serial(p[0], 9600, timeout=0)
+        # Faz a leitura e tratamento dos dados
+        dados = ser.readline()
+        linha = dados.decode("ansi")
+        if ";" in linha:
+            temp, um = linha.split(";")
+            temp = eval(temp)
+            um = eval(um)
+            chuva = 0    # Para uso futuro
+            if debug:
+                print("--\nTemperatura: %d ºC\nUmidade: %d %%\nChuva: %d" % (temp, um, chuva))
+            # Monta a URL e envia os dados para o webservice
+            params = urllib.parse.urlencode({'temp': temp, 'hum': um, 'rain': chuva})
+            url = "http://just.pro.br/ifba_sensors.py?%s" % params
+            conteudoweb = urllib.request.urlopen(url)
+            time.sleep(intervalo - 1)
+            
+            # Limpa o buffer de entrada para fazer uma nova leitura
+            ser.flushInput()
+    except serial.SerialException:
+        print("Erro ao fazer leitura serial.")
+    except urllib.error.URLError:
+        print("Erro ao fazer a requisição HTTP.")
+    time.sleep(1)
