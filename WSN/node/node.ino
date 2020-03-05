cf182a90b91850229d30ade312cb031d0700a2d1
João Paulo Just Peixoto <just1982@gmail.com>
2020-03-05 17:59:16 -0300

    Programa padrão para o nó WSN Arduino.

--- WSN/node/node.ino
+++ WSN/node/node.ino
@@ -3,6 +3,7 @@
 // Configurações do sensor DHT22
 #define DHT_PIN 8       // Porta digital
 #define DHT_TYPE DHT22  // Tipo
+#define ID "1"          // ID do sensor
 
 DHT dht(DHT_PIN, DHT_TYPE);
 
@@ -14,9 +15,15 @@ void setup()
 
 void loop()
 {
+  // Delay para a leitura do sensor
   delay(2000);
 
+  Serial.print(ID);
+  Serial.print(";");
   Serial.print(dht.readTemperature());
   Serial.print(";");
   Serial.println(dht.readHumidity());
+
+  // Dorme por 5 minutos
+  delay(300000);
 }
