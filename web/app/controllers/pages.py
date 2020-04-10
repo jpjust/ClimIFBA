7a06a9f39edd793350d47f20d3d1e080ed525faf
João Victor <jv.13wap@gmail.com>
2020-04-10 16:47:30 -0300

    Modularização do gráfico, Renomeação do default.py, Renomeação do def padrao(), Conversão de imagens para webp.

---
+++ web/app/controllers/pages.py
@@ -0,0 +1,18 @@
+from flask import request
+from flask import render_template
+from app.controllers.graphic import *
+from datetime import datetime, timedelta
+
+
+# Index
+def padrao():
+    # Datas para preenchimento do formulário
+    tz_local = pytz.timezone('America/Bahia')
+    if request.method != "POST":
+        dfinal = str(datetime.now().astimezone(tz_local)).replace(" ", "T")[:16]
+        dinicial = str((datetime.now() - timedelta(days=1)).astimezone(tz_local)).replace(" ", "T")[:16]
+    else:
+        dfinal = request.form["dfinal"]
+        dinicial = request.form["dinicial"]
+
+    return render_template("index.html", graph=Graphic(), dfinal=dfinal, dinicial=dinicial)
