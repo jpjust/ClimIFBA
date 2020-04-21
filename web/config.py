065a580c90235efc4d6cc7ba537c3d032655a655
João Victor <58078395+pupilo-py@users.noreply.github.com>
2020-04-21 19:09:51 -0300

    config.py consertado

--- web/config.py
+++ web/config.py
@@ -1,2 +1,2 @@
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = "mysql://ifba_sensors:iecdbp55@mysql.just.pro.br/ifba_sensors"
+SQLALCHEMY_DATABASE_URI = "mysql://xxxxx:xxxxx@mysql.just.pro.br/ifba_sensors"
